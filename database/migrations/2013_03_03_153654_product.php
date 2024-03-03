30039de9a1f1ded6507b47026dfcb04cbce1e21a
Ryu Orlando Tamin <ryutamin05@gmail.com>
2024-03-03 23:48:19 +0700

    Database setup, relationship, admin seeder, download breeze done

---
+++ database/migrations/2013_03_03_153654_product.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->string('picture');
+            $table->text('description');
+            $table->decimal('price', 9, 2)->default(0);
+            $table->integer('stock');
+            $table->string('category')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
